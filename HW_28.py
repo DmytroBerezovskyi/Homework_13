79ed9e7935cc47b3c39339e3897f46fe69c63f12
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-10-23 09:06:47 +0300

    ДЗ 27. Реверс списка

---
+++ HW_28.py
@@ -0,0 +1,15 @@
+
+lst = [int(s) for s in input('Введите пожалуйста список чисел: ').split()]
+
+if len(lst) % 2 == 0:
+    for i in range((len(lst))//2):                 # Количество таких обменов будет равно половине длины списка
+        lst.insert(len(lst)-i-1, lst.pop(i))
+        lst.insert(i, lst.pop(len(lst)-i-2))
+    print(lst)
+    print()
+else:
+    for i in range((len(lst))):                 # Иначе элементы поменяются местами по-второму кругу
+        lst.insert(len(lst)-i-1, lst.pop(i))    # и вернутся в первоначальное положение.
+        lst.insert(i, lst.pop(len(lst)-i-2))
+    print(lst)
+    print()
