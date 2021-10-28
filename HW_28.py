95febad59ab315036041e8b35210442d5a2364e0
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-10-28 16:49:19 +0300

    ДЗ 28. Конвертер чисел

--- HW_28.py
+++ HW_28.py
@@ -1,15 +1,75 @@
+num = int(input('Введите десятичное число: '))
+base = int(input('Введите систему исчисления (2-36): '))
+while True:
+    if 2 <= base <= 32:
+        break
+    else:
+        base = int(input('Введите систему исчисления (2-36): '))
+newNum = ''
+if base != 12 or base != 16 or base!=32:
+    while num > 0:
+        newNum = str(num % base) + newNum
+        num //= base
 
-lst = [int(s) for s in input('Введите пожалуйста список чисел: ').split()]
+    print(newNum)
 
-if len(lst) % 2 == 0:
-    for i in range((len(lst))//2):                 # Количество таких обменов будет равно половине длины списка
-        lst.insert(len(lst)-i-1, lst.pop(i))
-        lst.insert(i, lst.pop(len(lst)-i-2))
-    print(lst)
-    print()
-else:
-    for i in range((len(lst))):                 # Иначе элементы поменяются местами по-второму кругу
-        lst.insert(len(lst)-i-1, lst.pop(i))    # и вернутся в первоначальное положение.
-        lst.insert(i, lst.pop(len(lst)-i-2))
-    print(lst)
-    print()
+lst = []
+if base == 12:
+    while num > 0:
+        if num % base == 10:
+            lst.append('A')
+            num //= base
+            continue
+        if num % base == 11:
+            lst.append('B')
+            num //= base
+            continue
+        lst.append(str(num % base))
+        num //= base
+        if num < base:
+            if (num == 10):
+                lst.append('A')
+                break
+            if (num == 11):
+                lst.append('B')
+                break
+
+
+if base == 16:
+    while num > 0:
+        if num % base == 10:
+            lst.append('A')
+            num //= base
+            continue
+        if num % base == 11:
+            lst.append('B')
+            num //= base
+            continue
+        if num % base == 12:
+            lst.append('C')
+            num //= base
+            continue
+        if num % base == 13:
+            lst.append('D')
+            num //= base
+            continue
+        if num % base == 14:
+            lst.append('E')
+            num //= base
+            continue
+        if num % base == 15:
+            lst.append('F')
+            num //= base
+            continue
+        lst.append(str(num % base))
+        num //= base
+        if num < base:
+            if (num == 10):
+                lst.append('A')
+                break
+            if (num == 11):
+                lst.append('B')
+                break
+
+lst.reverse()
+print(''.join(lst))
