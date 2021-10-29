ef055efd1b0bb25e4477ac6d3ee50056bbdc460a
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-10-29 14:42:14 +0300

    ДЗ 28. Конвертер чисел

--- HW_27.py
+++ HW_27.py
@@ -1,15 +1,21 @@
-
 lst = [int(s) for s in input('Введите пожалуйста список чисел: ').split()]
-
 if len(lst) % 2 == 0:
-    for i in range((len(lst))//2):                 # Количество таких обменов будет равно половине длины списка
-        lst.insert(len(lst)-i-1, lst.pop(i))
-        lst.insert(i, lst.pop(len(lst)-i-2))
-    print(lst)
-
+    for i in range((len(lst))//2):
+        lst[i], lst[len(lst)-i-1] = lst[len(lst)-i-1], lst[i]
 else:
-    for i in range((len(lst))):                 # Иначе элементы поменяются местами по-второму кругу
-        lst.insert(len(lst)-i-1, lst.pop(i))    # и вернутся в первоначальное положение.
-        lst.insert(i, lst.pop(len(lst)-i-2))
-    print(lst)
+    for i in range((len(lst))):
+        lst[i], lst[len(lst)-i-1] = lst[len(lst)-i-1], lst[i]
+
+print(lst)
+# if len(lst) % 2 == 0:
+#     for i in range((len(lst))//2):                 # Количество таких обменов будет равно половине длины списка
+#         lst.insert(len(lst)-i-1, lst.pop(i))
+#         lst.insert(i, lst.pop(len(lst)-i-2))
+#     print(lst)
+#
+# else:
+#     for i in range((len(lst))):                 # Иначе элементы поменяются местами по-второму кругу
+#         lst.insert(len(lst)-i-1, lst.pop(i))    # и вернутся в первоначальное положение.
+#         lst.insert(i, lst.pop(len(lst)-i-2))
+#     print(lst)
 
